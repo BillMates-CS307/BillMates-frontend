e3465fb9f0534ee8a193674c348c36107f95beae
ChocoDino <coverlog555@gmail.com>
2023-02-19 14:15:29 -0500

    Password Validations
    Client side password validations:
    at least 3 lower case, 3 upper case, 3 numbers,  1 special character.
    Allow user to see password with eye click

--- pages/Forms/signup_form.js
+++ pages/Forms/signup_form.js
@@ -9,6 +9,22 @@ export default function PageWithJSbasedForm() {
         email: event.target.email.value,
         password: event.target.password.value,
       }
+
+      const field_check = checkFields(data);
+      if (!field_check.valid) {
+        for (let i in field_check.elms) {
+          field_check.elms[i].style = "outline: 1px solid #ff0101;";
+          field_check.elms[i].addEventListener('keydown', function () {
+            this.style = "";
+            this.nextElementSibling.style = "";
+          }, {once : true});
+          field_check.elms[i].nextElementSibling.textContent = field_check.messages[i];
+          field_check.elms[i].nextElementSibling.style = "display:block";
+        }
+        field_check.elms[0].focus();
+        return;
+      }
+
       const JSONdata = JSON.stringify(data)
       const endpoint = '/api/signin_api'
   
@@ -36,13 +52,38 @@ export default function PageWithJSbasedForm() {
       // }
     }
 
+    const checkFields = (target) => {
+      let output = {valid : true, elms : [], messages : []}
+      let regex = /[a-zA-z0-9]+@[a-zA-z0-9]+[.][a-zA-Z0-9]+/g;
+      if (target.email == "" || target.email.search(regex) == -1) {
+        output.elms.push(document.querySelector('#email'));
+        output.messages.push("Invalid Email");
+      }
+
+      if (target.password == "") {
+        output.elms.push(document.querySelector('#password'));
+        output.messages.push("Required");
+      }
+      regex = /[+^%\\'"]+/g;        
+      if (target.password.search(regex) != -1) {
+        output.elms.push(document.querySelector('#password'));
+        output.messages.push("Invalid Password");
+      }
+      if (output.elms.length > 0) {
+        output.valid = false;
+      }
+
+      return output;
+    }
+
     return (
 <>
     <div className={styles.signup_form}>
         <form onSubmit={handleSubmit} method="post">
             <div>
             <label htmlFor="email">Email:</label>
-            <input required type="email" id="email" name="email" />
+            <input type="text" id="email" name="email" />
+            <span></span>
             </div>
             <div>
             <label htmlFor="password">Password:</label>
@@ -50,8 +91,8 @@ export default function PageWithJSbasedForm() {
             type="password"
             id="password"
             name="password"
-            required
             />
+            <span></span>
             </div>
             <button type="submit">Sign In</button>
         </form>
