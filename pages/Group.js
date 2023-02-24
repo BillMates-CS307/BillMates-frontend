b0b99ddad526d835f6fb6c61ce6df72ca3627f2d
ChocoDino <coverlog555@gmail.com>
2023-02-23 23:24:09 -0500

    Sample Group Page
    Nothing dynamic, but some styling and what it could look like

---
+++ pages/Group.js
@@ -0,0 +1,354 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Group.module.css'
+
+export default function Group () {
+
+
+
+
+return (
+<>
+<main className={styles.main}>
+    <div className={styles.group_heading}>
+        <div className={styles.group_info}>
+            <div className={styles.names_members}>
+                <p>Baller Mates</p>
+                <p>Members: 3</p>
+            </div>
+            <div className={styles.group_amount}>
+                <p>$42.50</p>
+            </div>
+        </div>
+        <div className={styles.banner}>
+                <p>EXPENSES</p>
+        </div>
+    </div>
+    <div className={styles.transaction_history}>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>Dinner</p>
+                    <p>$60.00</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>Giannis Antetokounmpo</p>
+                    <p>02/23/2023</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$50.00</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>Starbies</p>
+                    <p>$10.00</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>Luka Doncic</p>
+                    <p>02/22/2023</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$7.50</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+        <div className={styles.transaction_container}>
+            <div className={styles.transaction_info}>
+                <div className={styles.transaction_name_amount}>
+                    <p>ITEM HERE</p>
+                    <p>$####.##</p>
+                </div>
+                <div className={styles.transaction_owner_date}>
+                    <p>NAME HERE</p>
+                    <p>##/##/####</p>
+                </div>
+            </div>
+            <div className={styles.relative_amount}>
+                <p>$####.##</p>
+            </div>
+        
+        </div>
+    </div>
+    
+</main>
+</>
+);
+}
