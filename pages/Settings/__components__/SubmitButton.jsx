de9d6f72e5a2be90c8ea2e0a8aeba0fad6212d82
wonho <biocle8339@gmail.com>
2023-02-26 12:21:57 -0500

    ADD request to settings_api

--- pages/Settings/__components__/SubmitButton.jsx
+++ pages/Settings/__components__/SubmitButton.jsx
@@ -1,22 +1,48 @@
 import React from "react";
-
 import styled from "@emotion/styled";
+import { useSelector } from "react-redux";
+import { selectsettingsData } from "@/lib/store/settingsData.slice";
 
 export default function SubmitButton() {
-  return <SubmitButtonWrapper>Submit</SubmitButtonWrapper>;
-}
+  const { name, password, notiPref } = useSelector(selectsettingsData);
+  const onClickSubmitHandler = async (e) => {
+    e.preventDefault();
+    // Get data from the form.
+    const data = {
+      name,
+      password,
+      notiPref,
+    };
 
-const UserInformationWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding-bottom: 10px;
-  border-radius: 10px;
-  box-shadow: 1px 2px 3px 0 #949494;
-  color: black;
-  overflow: hidden;
-`;
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/settings_api";
+
+    // Form the request for sending data to the server.
+    const options = {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+
+    if (response.status == 400) {
+      alert("Failed ");
+      return;
+    }
+
+    const result = await response.json();
+    console.log(result);
+  };
+
+  return (
+    <SubmitButtonWrapper onClick={onClickSubmitHandler}>
+      Submit
+    </SubmitButtonWrapper>
+  );
+}
 
 const SubmitButtonWrapper = styled.div`
   text-align: center;
---
+++ pages/api/settings_api.js
@@ -0,0 +1,39 @@
+export default async function handler(req, res) {
+  // Get data submitted in request's body.
+  const req_body = JSON.parse(req.body);
+
+  // Guard clause checks for first and last name,
+  // and returns early if they are not found
+  if (!req_body.name || !req_body.password || req_body.notification) {
+    // Sends a HTTP bad request error code
+    return res
+      .status(400)
+      .json({ data: "name, password, or notification not found" });
+  }
+
+  // Found the name.
+  // Sends a HTTP success code
+
+  //make request to Lambda
+  const body_json = {
+    name: req_body.name,
+    password: req_body.password,
+    notification: req_body.notification,
+  };
+  const url =
+    "https://yimbhwmzyzeikdbjqylkdonwoy0czhwq.lambda-url.us-east-2.on.aws/";
+  const options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      token: "zpdkwA.2_kLU@zg",
+    },
+    body: JSON.stringify(body_json),
+  };
+
+  const lambda_resp = await fetch(url, options);
+  const lambda_data = await lambda_resp.json();
+
+  return res.status(200).json(lambda_data);
+}
