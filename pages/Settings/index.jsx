8079b3db8b7502eeb62d42a9bb013b272a35f149
wonho <biocle8339@gmail.com>
2023-02-26 19:41:19 -0500

    Settings Added (#2)
    * ADD basic css for Settings
    
    * CONNECT redux
    
    * ADD Theme,
    FIX other components to deal with redux or local storage
    
    * ADD request to settings_api
    
    * ADD Header, Footer to Settings
    FIX settings api to contain email

---
+++ pages/Settings/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "@emotion/styled";
+import UserInformation from "./__components__/UserInformation";
+import Notification from "./__components__/Notification";
+import PaymentPreference from "./__components__/PaymentPreference";
+import SubmitButton from "./__components__/SubmitButton";
+import Theme from "./__components__/Theme";
+import Header from "../Globals/Header";
+import Footer from "../Globals/Footer";
+
+export default function Settings() {
+  return (
+    <>
+      <Header />
+      <SettingsWrapper>
+        <SettingsForm>
+          <UserInformation />
+          <Notification />
+          <PaymentPreference />
+          <Theme />
+          <SubmitButton />
+        </SettingsForm>
+      </SettingsWrapper>
+      <Footer />
+    </>
+  );
+}
+
+const SettingsWrapper = styled.div`
+  max-width: 440px;
+  margin: 0 auto;
+  padding: 1rem;
+  border-radius: 10px;
+  box-shadow: 1px 2px 15px 0 #949494;
+  background: #fff;
+`;
+
+const SettingsForm = styled.form``;
