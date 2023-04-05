4ad9feda19c8f0a0433f0cbdf95d953b8d0a070a
ChocoDino <coverlog555@gmail.com>
2023-04-05 18:42:59 -0400

    Learning Rechart

--- pages/groups/[id]/analytics.js
+++ pages/groups/[id]/analytics.js
@@ -12,15 +12,20 @@ import { user_methods } from '@/lambda_service/userService.js';
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/router.js';
 
+
+//Recharts items
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip, Legend } from 'recharts';
+
+
 //SHOW ANALYTICS HERE
 
-export default function ShoppingLists() {
+export default function Analytics() {
     //UNCOMMENT WHEN NEEDED
     const router = useRouter();
     const [isAuthenticated, setAuthentication] = useState(false);
     async function check() {
         //let result = await user_methods.validateLoginJWT(router);
-        let result = { success : true, payload : {"email" : "test@test.com"}};
+        let result = { success: true, payload: { "email": "test@test.com" } };
         if (result.success) {
             localStorage.setItem("tempId", result.payload.email);
             setAuthentication(true);
@@ -62,25 +67,137 @@ export default function ShoppingLists() {
         }
         return;
     }
+    const data = [
+        {
+            name: 'Page A',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'Page B',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'Page C',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: 'Page D',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'Page E',
+            uv: 1890,
+            pv: 4800,
+            amt: 2181,
+        },
+        {
+            name: 'Page F',
+            uv: 2390,
+            pv: 3800,
+            amt: 2500,
+        },
+        {
+            name: 'Page G',
+            uv: 3490,
+            pv: 4300,
+            amt: 2100,
+        },
+    ];
+    var galleryIdx = 0;
+    function advance() {
+        const target = document.querySelector("#here");
+        target.children[galleryIdx].style = "";
+        galleryIdx = (galleryIdx + 1) % 3;
+        target.children[galleryIdx].style = "display:block";
+    }
 
     if (isAuthenticated) {
         return (
             <>
                 <CustomHead title={"Analytics"} description={"Your group analytics"}></CustomHead>
-                <Header groupId = {groupSettingsRoute}></Header>
+                <Header groupId={groupSettingsRoute}></Header>
                 <main className={styles.main}>
-                { (loading)?
-                            <LoadingCircle additionalStyles={{ margin: "15px auto" }}></LoadingCircle>
-                            :
-                            <></>
-                }
+                    {(loading) ?
+                        <LoadingCircle additionalStyles={{ margin: "15px auto" }}></LoadingCircle>
+                        :
+                        <>
+   <button onClick={advance}>next chart</button>
+<div className={styles.testing} id="here">
+        <div className={styles.gallery_item_wrapper}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+        </div>
+        <div className={styles.gallery_item_wrapper}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="amt" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+        </div>
+        <div className={styles.gallery_item_wrapper}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="pv" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+        </div>
+      </div>
+                        </>
+                    }
                 </main>
 
 
                 <Footer></Footer>
             </>
 
-            
+
         )
     } else {
         return <></>
