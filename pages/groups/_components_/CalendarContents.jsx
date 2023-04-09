394c145847c827dc12a3cddffc366f7609c9a5fd
wonho <biocle8339@gmail.com>
2023-04-09 19:16:05 -0400

    FIX calendarData.slice, ADD calendar date tile the event registered show the dot

--- pages/groups/_components_/CalendarContents.jsx
+++ pages/groups/_components_/CalendarContents.jsx
@@ -1,19 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router.js";
 import styled from "@emotion/styled";
+import moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
 import Calendar from "react-calendar";
-import Header from "@/pages/global_components/groups_header";
-import Footer from "@/pages/global_components/footer";
-import CustomHead from "@/pages/global_components/head";
-import { CommonPopup } from "@/lib/ui/CommonPopup";
-import { selectGroupData } from "@/lib/store/groupData.slice";
+import {
+  selectCalendarData,
+  selectNonrecurringCalendarData,
+  selectRecurringCalendarData,
+} from "@/lib/store/calendarData/calendarData.slice";
 
 export default function CalendarContents() {
   const dispatch = useDispatch();
   const router = useRouter();
-  const groupData = useSelector(selectGroupData);
-  const [value, onChange] = useState(new Date());
+  const nonrecurringCalendarData = useSelector(selectNonrecurringCalendarData);
+  const recurringCalendarData = useSelector(selectRecurringCalendarData);
+  const [value, setValue] = useState(new Date());
+  // const [mark, setMark] = useState(["2023-04-10", "2023-04-10"]);
+
+  const onChange = (value, e) => {
+    setValue(e.target.value);
+    // TODO: Fix to detail page
+    // router.push("/");
+  };
+
+  const checkNonrecurringEventDay = (date) => {
+    for (const ev of nonrecurringCalendarData) {
+      if (
+        moment(ev.date).format("YYYY-MM-DD") ===
+        moment(date).format("YYYY-MM-DD")
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  const checkRecurringEventDay = (date) => {
+    for (const recurringEvent of recurringCalendarData) {
+      // console.log(moment(date).weekday());
+      if (moment(recurringEvent.date).weekday() === moment(date).weekday()) {
+        return true;
+      }
+    }
+
+    return false;
+  };
 
   return (
     <CalendarWrapper>
@@ -21,6 +54,18 @@ export default function CalendarContents() {
         calendarType="US"
         locale="en"
         onChange={onChange}
+        formatDay={(locale, date) => moment(date).format("DD")}
+        tileContent={({ date, view }) => {
+          if (checkRecurringEventDay(date) || checkNonrecurringEventDay(date)) {
+            return (
+              <>
+                <DotWrapper>
+                  <Dot />
+                </DotWrapper>
+              </>
+            );
+          }
+        }}
         value={value}
       />
     </CalendarWrapper>
@@ -33,7 +78,20 @@ const CalendarWrapper = styled.div`
   align-items: center;
 `;
 
-const Space = styled.div`
-  width: 100%;
-  height: 50px;
+const DotWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: absolute;
+  bottom: 4px;
+`;
+
+const Dot = styled.div`
+  height: 8px;
+  width: 8px;
+  background-color: #f87171;
+  border-radius: 50%;
+  display: flex;
+  margin-left: 1px;
 `;
