1eb0683679452aa52e2525bf85bd86929f41fe98
wonho <biocle8339@gmail.com>
2023-04-18 23:46:53 -0400

    ADD User Calendar

---
+++ pages/settings/user_calendar/index.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router.js";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectUserData,
+  userDataAction,
+} from "@/lib/store/userData/userData.slice";
+import { user_methods } from "@/lambda_service/userService";
+import Header from "@/pages/global_components/header";
+import Footer from "@/pages/global_components/footer";
+import CustomHead from "@/pages/global_components/head";
+import { CommonPopup } from "@/lib/ui/CommonPopup";
+import { calendarDataAction } from "@/lib/store/calendarData/calendarData.slice";
+import getUserCalendar from "@/lib/api/getUserCalendar";
+import CalendarContents from "../__components__/CalendarContents";
+import LoadingCircle from "../../global_components/loading_circle";
+
+let calendarData = {
+  events: [],
+};
+
+export default function GroupCalendar() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isAuthenticated, setAuthentication] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const userId = isAuthenticated ? localStorage.getItem("tempId") : null;
+
+  async function check() {
+    let result = await user_methods.validateLoginJWT(router);
+    if (result.success) {
+      localStorage.setItem("tempId", result.payload.email);
+      setAuthentication(true);
+    }
+  }
+
+  async function fetchData() {
+    console.log("fetching data");
+    let lambda_resp = await getUserCalendar({
+      email: userId,
+    });
+    console.log("resp");
+    console.log(lambda_resp);
+    if (lambda_resp.token_success && lambda_resp.get_success) {
+      calendarData = lambda_resp.events;
+      dispatch(calendarDataAction.setCalendarData({ events: calendarData }));
+      console.log(calendarData);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      console.log("authenticating");
+      check();
+    }
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchData(); //make the call
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <>
+      <CustomHead
+        title={"User Calendar"}
+        description={"A BillMates calendar"}
+      ></CustomHead>
+      <CommonPopup />
+      <Header />
+      <CalendarWrapper>
+        {loading ? (
+          <LoadingCircle
+            additionalStyles={{ margin: "15px auto" }}
+          ></LoadingCircle>
+        ) : (
+          <>
+            <CalendarContents />
+          </>
+        )}
+        <Space />
+      </CalendarWrapper>
+      <Footer />
+    </>
+  );
+}
+
+const CalendarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 700px;
+  width: 90%;
+  margin: 0 auto;
+  padding: 1rem;
+  border-radius: var(--border-radius);
+  box-shadow: 1px 2px 15px 0 #949494;
+  color: var(--main-background-font-color);
+  background: var(--main-background);
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 50px;
+  padding: 10px 20px;
+`;
+
+const AddLink = styled(Link)`
+  padding: 5px;
+  background-color: var(--green-background);
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const Space = styled.div`
+  width: 100%;
+  height: 50px;
+`;
---
+++ pages/settings/user_calendar_detail/index.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router.js";
+import styled from "@emotion/styled";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
+import { user_methods } from "@/lambda_service/userService";
+import Header from "@/pages/global_components/header";
+import Footer from "@/pages/global_components/footer";
+import CustomHead from "@/pages/global_components/head";
+import { CommonPopup } from "@/lib/ui/CommonPopup";
+import { selectCalendarData } from "@/lib/store/calendarData/calendarData.slice";
+import CalendarDetailItem from "../_components_/CalendarDetailItem";
+import { checkEventDayBy } from "@/lib/util/date";
+
+export default function GroupCalendarDetail() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isAuthenticated, setAuthentication] = useState(false);
+  const calendarData = useSelector(selectCalendarData);
+  const eventsSortedByTime = calendarData.events
+    .filter(checkEventDayBy(router.query.date))
+    .sort(({ time: a }, { time: b }) => (a > b ? 1 : a < b ? -1 : 0));
+  const userId = isAuthenticated ? localStorage.getItem("tempId") : null;
+
+  console.log("calendar detail");
+  console.log(eventsSortedByTime);
+  async function check() {
+    let result = await user_methods.validateLoginJWT(router);
+    if (result.success) {
+      localStorage.setItem("tempId", result.payload.email);
+      setAuthentication(true);
+    }
+  }
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      console.log("authenticating");
+      check();
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <>
+      <CustomHead
+        title={"Calendar Detail"}
+        description={"A BillMates calendar detail"}
+      />
+      <CommonPopup />
+      <Header />
+      <CalendarWrapper>
+        {eventsSortedByTime.map((ev, i) => (
+          <CalendarDetailItem key={i} {...ev} />
+        ))}
+        <Space />
+      </CalendarWrapper>
+      <Footer />
+    </>
+  );
+}
+
+const CalendarWrapper = styled.div`
+  max-width: 700px;
+  width: 90%;
+  margin: 0 auto;
+  padding: 1rem;
+  border-radius: var(--border-radius);
+  box-shadow: 1px 2px 15px 0 #949494;
+  color: var(--main-background-font-color);
+  background: var(--main-background);
+`;
+
+const CalendarTitleWrapper = styled.div`
+  padding: 10px;
+  width: 100%;
+  background: var(--green-background);
+  color: var(--main-background-font-color);
+`;
+
+const CalendarTitle = styled.h3``;
+
+const Space = styled.div`
+  width: 100%;
+  height: 50px;
+`;
